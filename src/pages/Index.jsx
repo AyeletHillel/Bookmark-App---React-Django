19bf4760e7236fcc76f43535ef4f9abbf15bad09
Ayelet Hillel <ah3708@tc.columbia.edu>
2023-02-22 21:56:31 -0500

    data displayed on index page

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -6,7 +6,12 @@ const Index = () => {
     return (<div>
         <Navbar/>
         <Header/>
+        <section class="bg-[#F3F4F6] pt-20 pb-10 lg:pt-[120px] lg:pb-20">
+        <div class="container mx-auto">
+        <div class="-mx-4 flex flex-wrap"></div>
         <Cards/>
+        </div>
+        </section>
         </div>
     )
 };
